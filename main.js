0783797afbad0043c80efc90029ee6289d3df5e0
Karthikeyan S <154907998+karthikns03@users.noreply.github.com>
2023-12-31 23:07:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function submitForm() {
+  var userName = document.getElementById("userName").value;
+  var greetingText = document.getElementById("greeting-text");
+  var audio = document.getElementById("audio");
+  var resultImage = document.getElementById("result-image");
+  var resultSection = document.getElementById("result-section");
+
+  // Display greeting text
+  // Display audio and image
+  audio.src = "ந பறநதத வஸட [TubeRipper.com].mp3";
+  resultImage.src = "meme.jpg";
+  greetingText.textContent =
+    "Fuck You, " +
+    userName +
+    " ===>Saavu da punda<=== Unake NEW YEAR oru kedu" +
+    "!!!";
+
+  // Show result section
+  resultSection.style.display = "block";
+}
